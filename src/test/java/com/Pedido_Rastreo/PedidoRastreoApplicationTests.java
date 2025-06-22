566f338e9033bde7c2eddf59f52106342fb96a24
Ceball0s <ceballosmiguel44@gmail.com>
2025-06-21 21:22:18 -0500

    first commit

---
+++ src/test/java/com/Pedido_Rastreo/PedidoRastreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pedido_Rastreo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoRastreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
